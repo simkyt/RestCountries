780318daad5cc363c359cb5727c2841e3e633e60
simkyt <99974435+simkyt@users.noreply.github.com>
2023-11-16 21:05:06 +0200

    Initial Commit

---
+++ RestCountries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestCountries
+//
+//  Created by Simonas Kytra on 16/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
